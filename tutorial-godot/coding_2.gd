844ccc875af374c4cd3de93411bf24e83d909ebf
Fon5eca <fonseblad@gmail.com>
2026-03-02 21:31:31 -0600

    Métodos y propiedades

--- tutorial-godot/coding_2.gd
+++ tutorial-godot/coding_2.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-func _process(delta: float):
-	$Sprite2D.rotate(0.01)
-	$Sprite2D.position.x = $Sprite2D.position.x + 0.5
-	$Sprite2D.position.y = $Sprite2D.position.y + 0.5
+func _process(_delta: float):
+	$Sprite2D.scale.x += 0.0002
+	$Sprite2D.scale.y += 0.0002
+	$Sprite2D.position.x += 0.5
+	$Sprite2D.position.y += 0.265
